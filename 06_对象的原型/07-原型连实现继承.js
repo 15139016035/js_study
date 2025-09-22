07cb0ef7bb4c3cea8425826081e7b03df0ca12db
武耀华 <460615310@qq.com>
2025-09-23 00:15:46 +0800

    原型链实现继承

---
+++ 06_对象的原型/07-原型连实现继承.js
@@ -0,0 +1,61 @@
+/**
+ * 通过原型连实现继承的 一个案例
+ */
+
+
+/**
+ * 父类Person
+ */
+
+function Person(name, age) {
+    this.name = name;
+    this.age = age;
+
+}
+
+Person.prototype.running = function () {
+    console.log(this.name + "在跑步～～");
+}
+
+/**
+ * 子类。Studetn 和 Teatcher
+ */
+
+function Student(name, age, score) {
+    //间接的实现属性的继承
+    /**
+     * 这种实现属性继承的逻辑：
+     *   let stu = new Student();  此时this就是stu实例对象；
+     *   然后通过call调用了Person，Person内部就会执行代码，Person内部的this就是stu
+     *   就是stu.name =name ,stu.age =age;
+     */
+    Person.call(this,name,age);
+    this.score = score;
+}
+
+Student.prototype.study = function () {
+    console.log("学生在学习");
+}
+
+Student.prototype.__proto__=Person.prototype;
+
+function Teacher(name,age){
+    //间接的实现属性的继承
+    Person.call(this,name,age);
+
+}
+
+Teacher.prototype.teatch=function(){
+    console.log("老师在教学")
+}
+Teacher.prototype.__proto__=Person.prototype;
+
+
+let teacher = new Teacher("张老师");
+teacher.running();
+teacher.teatch();
+
+let student = new Student("王同学");
+student.running();
+student.study();
+
